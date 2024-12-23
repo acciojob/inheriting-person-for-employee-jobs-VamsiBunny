b6e3fcc373fc4b1000556033c0583992a3ac51ec
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-12-23 18:38:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-// complete this js code
-function Person(name, age) {}
+// Create the Person class
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+// Add the greet method to the Person class
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+// Create the Employee class that extends the Person class
+function Employee(name, age, jobTitle) {
+  // Call the Person constructor to set the name and age
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+// Inherit methods from Person
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+// Add the jobGreet method to the Employee class
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 
 // Do not change code below this line
 window.Person = Person;
